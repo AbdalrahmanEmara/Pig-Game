c74391a6bcd0a36a8f7e49d4be55b5816d240fb4
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-04-17 21:28:53 +0200

    Set Initial Conditions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting Elements
+const score1EL = document.querySelector('#score--0');
+const score2EL = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const rollBtn = document.querySelector('.btn--roll');
+
+// Starting Conditions
+score1EL.textContent = 0;
+score2EL.textContent = 0;
+diceEl.classList.add('hidden');
+
