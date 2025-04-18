714cdac36d05e9376791e5dc00c9c3895c37769d
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-04-18 09:28:32 +0200

    switching player if dice 1 appears

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score1EL = document.querySelector('#score--0');
 const score2EL = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -11,7 +13,9 @@ const btnNew = document.querySelector('.btn--roll');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--roll');
 
+let scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting Conditions
 score1EL.textContent = 0;
@@ -31,12 +35,17 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
-
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // switch to next player
-
-  }
+    // reset values
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    // switch players
+    activePlayer = activePlayer === 0 ? 1 : 0;    
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  } 
 
 })
 
